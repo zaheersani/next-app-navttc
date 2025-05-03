db63746b9c09312005a4c5e2fe7374efaf334d94
Zaheer Sani <sani.zaheer@gmail.com>
2025-05-03 10:47:33 +0500

    Moved People Component in File

--- src/app/page.js
+++ src/app/page.js
@@ -3,48 +3,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-
-const people = [{
-  id: 0,
-  name: 'Creola Katherine Johnson',
-  profession: 'mathematician',
-}, {
-  id: 1,
-  name: 'Mario José Molina-Pasquel Henríquez',
-  profession: 'chemist',
-}, {
-  id: 2,
-  name: 'Mohammad Abdus Salam',
-  profession: 'physicist',
-}, {
-  id: 3,
-  name: 'Percy Lavon Julian',
-  profession: 'chemist',  
-}, {
-  id: 4,
-  name: 'Subrahmanyan Chandrasekhar',
-  profession: 'astrophysicist',
-}];
-
-const PeopleList = () => {
-  return (
-    <div>
-      <h2>People</h2>
-      {console.log("List of Persons")}
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Profession</th>
-        </tr>
-        {people.map( (p, i) => <tr key={p.id}>
-                            <td className={styles.td}>{i+1}: {p.name}</td>
-                            <td className={styles.td}>{p.profession}</td>
-                            </tr>
-                  )}
-      </table>
-    </div>
-  )
-}
+import PeopleList from "./people.js";
 
 const Graduated = ({year}) => {
   return (

3a2460ca0c71950782b43332d56ca35992f41586
Zaheer Sani <sani.zaheer@gmail.com>
2025-05-30 18:05:59 +0500

    WeatherAPI

--- src/app/rapidapi/location/page.jsx
+++ src/app/rapidapi/location/page.jsx
@@ -2,9 +2,12 @@
 
 import { useEffect, useState } from "react";
 
+const APIKEY = process.env.NEXT_PUBLIC_RAPIDAPI_KEY;
+
 export default function Location() {
 
     const [position, setPosition] = useState(null);
+    const [weather, setWeather] = useState(null);
 
     const options = {
         enableHighAccuracy: true,
@@ -36,6 +39,36 @@ export default function Location() {
             return;
         }
     }, []);
+
+    useEffect(() => {
+      if(position === null) {
+        console.log("Waiting for position...");
+        return;
+      }
+
+      // Make the weather API call here
+      let lat = position.latitude;
+      let lon = position.longitude;
+      const url = `https://open-weather13.p.rapidapi.com/latlon?latitude=${lat}&longitude=${lon}&lang=EN`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': APIKEY, // <-- Replace with your RapidAPI key
+          'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = fetch(url, options).then(res => res.json()).then(data => {
+          console.log(data);
+          setWeather(data);
+        });
+        // const result = await response.json();
+        // console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }, [position]);
     
 
   return (
@@ -53,6 +86,20 @@ export default function Location() {
       <br />
       <p>This page retrieves your current geographical location using the Geolocation API.</p>
       <p>Check the console for your location details.</p>
+      
+      <h1>Weather of {weather && weather.name}</h1>
+      {weather && (
+        <div>
+          <h2>Weather Details:</h2>
+          <p>
+            Temperature: {weather.main && (weather.main.temp - 273.15).toFixed(2)}°C
+          </p>
+          <p>Humidity: {weather.main && weather.main.humidity}%</p>
+          <p>Weather: {weather.weather && weather.weather[0].description}</p>
+          <p>Min Temperature: {(weather.main.temp_min - 273.15).toFixed(2)}°C </p>
+        </div>
+      )}
+      {!weather && <p>Loading weather data...</p>}
     </div>
   );
 }
